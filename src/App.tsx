a69100f6a5ce007f6808f8f4a33b2a3a570bf5f0
Zakaria-Fettah <zakariafettah52@gmail.com>
2026-04-20 19:54:43 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,8 @@
-
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./contexts/AuthContext";
 import ProtectedRoute from "./components/ProtectedRoute";
 
@@ -30,15 +29,13 @@ const App = () => (
       <BrowserRouter>
         <AuthProvider>
           <Routes>
-            {/* Public Routes */}
+            {/* 1. Index hiya awal haja t-ban (Public daba) */}
+            <Route path="/" element={<Index />} />
+
+            {/* 2. Login t-khlliha ila bghiti t-testerha men be3d */}
             <Route path="/login" element={<Login />} />
             
-            {/* Protected Routes */}
-            <Route path="/" element={
-              <ProtectedRoute>
-                <Index />
-              </ProtectedRoute>
-            } />
+            {/* 3. L-baqi dyal l-pages (ila bghiti t-hiyed lihom ProtectedRoute hta homa) */}
             <Route path="/employees" element={
               <ProtectedRoute>
                 <Employees />
@@ -80,7 +77,6 @@ const App = () => (
               </ProtectedRoute>
             } />
             
-            {/* 404 Not Found */}
             <Route path="*" element={<NotFound />} />
           </Routes>
         </AuthProvider>
